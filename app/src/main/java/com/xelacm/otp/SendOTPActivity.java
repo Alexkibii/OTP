42d2da36dd2bef88a1599e9fc065f9740afe7780
Alexkibii <alexkibii@gmail.com>
2021-02-22 01:21:57 +0300

    initial commit

---
+++ app/src/main/java/com/xelacm/otp/SendOTPActivity.java
@@ -0,0 +1,100 @@
+package com.xelacm.otp;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.Toast;
+
+import com.google.firebase.FirebaseException;
+import com.google.firebase.FirebaseTooManyRequestsException;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseAuthInvalidCredentialsException;
+import com.google.firebase.auth.PhoneAuthCredential;
+import com.google.firebase.auth.PhoneAuthOptions;
+import com.google.firebase.auth.PhoneAuthProvider;
+
+import java.util.concurrent.TimeUnit;
+
+public class SendOTPActivity extends AppCompatActivity {
+
+     PhoneAuthProvider.OnVerificationStateChangedCallbacks mCallbacks;
+     FirebaseAuth mAuth;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_send_o_t_p);
+        final EditText inputMobile = findViewById(R.id.inputMobile);
+        Button buttonGetOTP = findViewById(R.id.buttonGetOTP);
+
+        ProgressBar progressBar = findViewById(R.id.progressbar);
+        mAuth = FirebaseAuth.getInstance();
+        buttonGetOTP.setOnClickListener(view -> {
+            if(inputMobile.getText().toString().isEmpty()){
+                Toast.makeText(SendOTPActivity.this, "Mobile Number Can't be Empty", Toast.LENGTH_LONG).show();
+                return;
+            }
+
+            progressBar.setVisibility(View.VISIBLE);
+            buttonGetOTP.setVisibility(View.INVISIBLE);
+
+            PhoneAuthOptions options =
+                    PhoneAuthOptions.newBuilder(mAuth)
+                            .setPhoneNumber("+254"+inputMobile.getText().toString())       // Phone number to verify
+
+                            .setTimeout(60L, TimeUnit.SECONDS) // Timeout and unit
+                            .setActivity(this)                 // Activity (for callback binding)
+                            .setCallbacks(new PhoneAuthProvider.OnVerificationStateChangedCallbacks() {
+
+                                @Override
+                                public void onVerificationCompleted(PhoneAuthCredential credential) {
+                                    // This callback will be invoked in two situations:
+                                    // 1 - Instant verification. In some cases the phone number can be instantly
+                                    //     verified without needing to send or enter a verification code.
+                                    // 2 - Auto-retrieval. On some devices Google Play services can automatically
+                                    //     detect the incoming verification SMS and perform verification without
+                                    //     user action.
+                                    progressBar.setVisibility(View.GONE);
+                                    buttonGetOTP.setVisibility(View.VISIBLE);
+
+                                }
+
+                                @Override
+                                public void onVerificationFailed(FirebaseException e) {
+                                    progressBar.setVisibility(View.GONE);
+                                    buttonGetOTP.setVisibility(View.VISIBLE);
+                                    Toast.makeText(SendOTPActivity.this,e.getMessage(),Toast.LENGTH_LONG).show();
+                                }
+
+                                @Override
+                                public void onCodeSent(@NonNull String verificationId,
+                                                       @NonNull PhoneAuthProvider.ForceResendingToken token) {
+                                    // The SMS verification code has been sent to the provided phone number, we
+                                    // now need to ask the user to enter the code and then construct a credential
+                                    // by combining the code with a verification ID.
+                                    progressBar.setVisibility(View.GONE);
+                                    buttonGetOTP.setVisibility(View.VISIBLE);
+
+                                    Intent intent = new Intent(getApplicationContext(), VerifyOTPActivity.class);
+                                    intent.putExtra("mobile", inputMobile.getText().toString());
+                                    intent.putExtra("verificationId", verificationId);
+                                    startActivity(intent);
+                                }
+                            })          // OnVerificationStateChangedCallbacks
+                            .build();
+            PhoneAuthProvider.verifyPhoneNumber(options);
+
+
+
+
+            });
+    }
+
+}
---
+++ app/src/main/java/com/xelacm/otp/VerifyOTPActivity.java
@@ -0,0 +1,181 @@
+package com.xelacm.otp;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.PhoneAuthCredential;
+import com.google.firebase.auth.PhoneAuthProvider;
+
+public class VerifyOTPActivity extends AppCompatActivity {
+
+private EditText inputCode1, inputCode2, inputCode3, inputCode4, inputCode5, inputCode6;
+private String verificationId;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_verify_o_t_p);
+
+        TextView textMobile = findViewById(R.id.textMobile);
+        inputCode1 = findViewById(R.id.inputCode1);
+        inputCode2 = findViewById(R.id.inputCode2);
+        inputCode3 = findViewById(R.id.inputCode3);
+        inputCode4 = findViewById(R.id.inputCode4);
+        inputCode5 = findViewById(R.id.inputCode5);
+        inputCode6 = findViewById(R.id.inputCode6);
+setUpOtpInputs();
+final ProgressBar progressBar = findViewById(R.id.progressbar);
+final Button verifyBtn = findViewById(R.id.buttonVerify);
+verificationId = getIntent().getStringExtra("verificationId");
+textMobile.setText(getIntent().getStringExtra("mobile"));
+verifyBtn.setOnClickListener(new View.OnClickListener() {
+    @Override
+    public void onClick(View view) {
+        if(inputCode1.getText().toString().trim().isEmpty()||
+           inputCode2.getText().toString().trim().isEmpty()||
+        inputCode3.getText().toString().trim().isEmpty()||
+        inputCode4.getText().toString().trim().isEmpty()||
+                inputCode5.getText().toString().trim().isEmpty()||
+                inputCode6.getText().toString().trim().isEmpty()){
+            Toast.makeText(VerifyOTPActivity.this,"Enter Valid code", Toast.LENGTH_LONG).show();
+            return;
+        }
+        String code = inputCode1.getText().toString() +
+                      inputCode2.getText().toString() +
+                      inputCode3.getText().toString() +
+                inputCode4.getText().toString() +
+                inputCode5.getText().toString() +
+                inputCode6.getText().toString();
+if (verificationId !=  null){
+    progressBar.setVisibility(View.VISIBLE);
+    verifyBtn.setVisibility(View.INVISIBLE);
+    PhoneAuthCredential phoneAuthCredential = PhoneAuthProvider.getCredential(verificationId, code);
+    FirebaseAuth.getInstance().signInWithCredential(phoneAuthCredential).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+        @Override
+        public void onComplete(@NonNull Task<AuthResult> task) {
+            progressBar.setVisibility(View.GONE);
+            verifyBtn.setVisibility(View.VISIBLE);
+            if(task.isSuccessful()){
+                Toast.makeText(VerifyOTPActivity.this, "Verification passed", Toast.LENGTH_LONG).show();
+            }else{
+                Toast.makeText(VerifyOTPActivity.this, "Invalid code", Toast.LENGTH_LONG).show();
+            }
+        }
+    });
+}
+    }
+});
+
+    }
+
+    private void setUpOtpInputs(){
+
+        inputCode1.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                if(!charSequence.toString().isEmpty()){
+                    inputCode2.requestFocus();
+                }
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+
+            }
+        });
+        inputCode2.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                if(!charSequence.toString().isEmpty()){
+                    inputCode3.requestFocus();
+                }
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+
+            }
+        });
+        inputCode3.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                if(!charSequence.toString().isEmpty()){
+                    inputCode4.requestFocus();
+                }
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+
+            }
+        });
+        inputCode4.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                if(!charSequence.toString().isEmpty()){
+                    inputCode5.requestFocus();
+                }
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+
+            }
+        });
+        inputCode5.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                if(!charSequence.toString().isEmpty()){
+                    inputCode6.requestFocus();
+                }
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+
+            }
+        });
+
+
+
+    }
+}
